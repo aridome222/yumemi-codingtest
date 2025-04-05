0e7809eef3f1b353822be5e235d29bbb6bece465
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-06 01:44:50 +0900

    refactor: リンター&フォーマットを適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 export default function Home() {
   return (
-    <div className="text-blue-400 text-4xl font-bold flex justify-center items-center h-screen">
+    <div
+      className={`
+        flex h-screen items-center justify-center text-4xl font-bold text-blue-400
+      `}
+    >
       Hello World!
     </div>
   );
