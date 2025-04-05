5bcf3cadf90759af67fd58dd4b1300ae069f9cac
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-06 01:23:23 +0900

    add: formatコマンドを追加・適用

--- prettierrc.config.mjs
+++ prettierrc.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('prettier').Config} */
 export default {
-    trailingComma: 'all', // 末尾のカンマあり
-    tabWidth: 4, // タブ幅 4
-    semi: true, // セミコロンあり
-    singleQuote: true, // シングルクォートに統一
-    jsxSingleQuote: true, //　JSXもシングルクォートに統一
-    printWidth: 90, // １行の最大文字数 90
+  trailingComma: "all", // 末尾のカンマあり
+  tabWidth: 4, // タブ幅 4
+  semi: true, // セミコロンあり
+  singleQuote: true, // シングルクォートに統一
+  jsxSingleQuote: true, //　JSXもシングルクォートに統一
+  printWidth: 90, // １行の最大文字数 90
 };
