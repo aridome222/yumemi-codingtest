9ae4008575a427dcbc33eae86cbd480951563609
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-10 17:33:10 +0900

    add: 都道府県一覧取得ボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client';
+
 export default function Home() {
     return (
-        <div className='flex h-screen items-center justify-center text-4xl font-bold text-blue-400'>
-            Hello World!
-        </div>
+        <>
+            <h1 className='mt-6 ml-3'>都道府県別の総人口推移グラフを表示する</h1>
+            <button
+                type='button'
+                className='rounded bg-blue-500 py-2 pr-2 m-6 text-xl font-bold text-white'
+                onClick={() => console.log('clicked')}
+            >
+                「都道府県一覧」取得
+            </button>
+        </>
     );
 }
