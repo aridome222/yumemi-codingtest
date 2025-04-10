0ac78b66a9ee7cce8631bddc9ead582ac16dd318
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-11 01:35:21 +0900

    add: 都道府県一覧データの型を定義

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+type PrefectureData = {
+    prefCode: number;
+    prefName: string;
+};
+
+type PrefectureResponce = {
+    message: string;
+    result: PrefectureData[];
+};
+
 export default function Home() {
     const url = process.env.NEXT_PUBLIC_RESAS_API_URL || '';
     const apiKey = process.env.NEXT_PUBLIC_RESAS_API_KEY || '';
