29ba0bb6dfc6e6823a0abadd5383cb9894d6e407
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-12 23:47:26 +0900

    update: クライアントが自前のAPIを経由して都道府県一覧データを取得できるようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+
 import type { PrefectureData } from '@/types/prefecture/prefectureData';
 
 export default function Home() {
-    const url = process.env.NEXT_PUBLIC_RESAS_API_URL || '';
-    const apiKey = process.env.NEXT_PUBLIC_RESAS_API_KEY || '';
-
-    if (!url || !apiKey) {
-        throw new Error('APIのURL、または、APIキーが設定されていません');
-    }
-
     const [prefectures, setPrefectures] = useState<PrefectureData[]>([]);
     const [error, setError] = useState<string | null>(null);
+
+    async function getPrefectures() {
+        try {
+            const res = await fetch('/api/prefectures');
+
+            if (!res.ok) {
+                const { error } = await res.json();
+                setError(error);
+                return;
+            }
+
+            const resJson = await res.json();
+            const prefectures: PrefectureData[] = resJson.prefectures;
+            setPrefectures(prefectures);
+        } catch (error) {
+            setError('通信に失敗しました。ネットワーク接続を確認してください。');
+        }
+    }
 
     useEffect(() => {
         if (prefectures.length === 47) {
@@ -20,42 +32,13 @@ export default function Home() {
         }
     }, [prefectures]);
 
-    const fetchPrefectures = async () => {
-        try {
-            const res = await fetch(`${url}/api/v1/prefectures`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-API-KEY': apiKey,
-                },
-            });
-
-            if (!res.ok) {
-                throw new Error(
-                    `都道府県一覧データの取得に失敗しました。status code: ${res.status}`,
-                );
-            }
-
-            const data: PrefectureResponce = await res.json();
-            setPrefectures(data.result);
-            setError(null);
-        } catch (error) {
-            if (error instanceof Error) {
-                console.error('都道府県一覧データ取得時エラー:', error.message);
-                setError(error.message);
-            } else {
-                console.error('不明なエラーが発生しました');
-                setError('不明なエラーが発生しました');
-            }
-        }
-    };
-
     return (
         <>
             <h1 className='mt-6 ml-3'>都道府県別の総人口推移グラフを表示する</h1>
             <button
                 type='button'
                 className='m-6 rounded bg-blue-500 py-2 pr-2 text-xl font-bold text-white'
-                onClick={fetchPrefectures}
+                onClick={getPrefectures}
             >
                 「都道府県一覧」取得
             </button>
