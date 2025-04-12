627e9f42f826bc6e4e2c1f36c3886c778e576679
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-12 23:43:29 +0900

    add: APIキーを隠蔽するために、ルートハンドラーを用いて都道府県一覧データ取得APIを作成

---
+++ src/app/api/prefectures/route.ts
@@ -0,0 +1,61 @@
+export const dynamic = 'force-static';
+
+import { NextResponse } from 'next/server';
+
+import type {
+    PrefectureResponce,
+    PrefectureData,
+} from '@/types/prefecture/prefectureData';
+
+/**
+ * 都道府県一覧データ取得API
+ *
+ * 成功時: { prefectures: PrefectureData[] }
+ * 失敗時: { error: string } と HTTPステータスコード（例: 400, 401, 500 など）
+ *
+ * 外部API（RESAS）へのリクエストを行い、整形済みのデータを返す。
+ */
+export async function GET(): Promise<NextResponse> {
+    const apiUrl = process.env.NEXT_PUBLIC_RESAS_API_URL || '';
+    const apiKey = process.env.NEXT_PUBLIC_RESAS_API_KEY || '';
+
+    if (!apiUrl || !apiKey) {
+        return NextResponse.json(
+            {
+                error: 'APIのURL、または、APIキーが設定されていません',
+            },
+            { status: 500 },
+        );
+    }
+
+    try {
+        const res = await fetch(`${apiUrl}/api/v1/prefectures`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-KEY': apiKey,
+            },
+        });
+
+        if (!res.ok) {
+            return NextResponse.json(
+                {
+                    error: `${res.statusText}: 都道府県一覧データの取得に失敗しました。`,
+                },
+                { status: res.status },
+            );
+        }
+
+        const prefectureResponce: PrefectureResponce = await res.json();
+        const prefectures: PrefectureData[] = prefectureResponce.result;
+        // { prefectures: [...] } というJSONを含むレスポンスを返す
+        return NextResponse.json({ prefectures });
+    } catch (error) {
+        // fetchが失敗した時の処理
+        return NextResponse.json(
+            {
+                error: `${error}: 都道府県一覧データの取得時にエラーが発生しました`,
+            },
+            { status: 500 },
+        );
+    }
+}
