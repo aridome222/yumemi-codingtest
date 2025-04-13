b3d6ed0b788c8268b8b2efbc9c8ac3feee26e5d9
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-13 22:15:29 +0900

    update: ボタンクリックではなく、初期ロード時に都道府県一覧データを取得するように実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,15 @@ export default function Home() {
     };
 
     useEffect(() => {
+        loadPrefectures();
         if (prefectures.length === 47) {
             console.log('都道府県一覧データを取得できました:', prefectures);
         }
-    }, [prefectures]);
+    }, []);
 
     return (
         <>
             <h1 className='mt-6 ml-3'>都道府県別の総人口推移グラフを表示する</h1>
-            <button
-                type='button'
-                className='m-6 rounded bg-blue-500 py-2 pr-2 text-xl font-bold text-white'
-                onClick={loadPrefectures}
-            >
-                「都道府県一覧」取得
-            </button>
 
             {error && <p className='m-3 text-red-500'>エラー: {error}</p>}
 
