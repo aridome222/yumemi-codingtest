72e605bcf9498f433f73501d500ab9b8459df7a6
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-04-13 20:14:51 +0900

    update: エラー種別とそれに対応するエラーメッセージの組を定義し、管理・変更をしやすくした

--- src/utils/prefecture/fetchPrefectures.ts
+++ src/utils/prefecture/fetchPrefectures.ts
@@ -1,5 +1,17 @@
 import type { PrefectureData } from '@/types/prefecture/prefectureData';
 
+const errorMessages: Record<string, string> = {
+    missing_env: 'APIキーまたはURLの環境変数が設定されていません。',
+    bad_request: 'APIに必要なパラメータが正しく設定されていません。',
+    forbidden: 'APIキーが存在しないか無効です。',
+    not_found: '指定されたURLに対応するAPIが存在しません。',
+    server_error: '外部APIサーバー側で問題が発生しています。',
+    resas_api_fetch_failed:
+        '都道府県一覧データの取得時にネットワークエラーが発生しました。',
+    internal_server_error: 'サーバーに問題が発生しています。',
+    unknown_error: '不明なエラーが発生しました。',
+};
+
 export async function fetchPrefectures(): Promise<{
     data: PrefectureData[] | null;
     error: string | null;
